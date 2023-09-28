25ecd8bc74076677b42b131e4aa8ce546941e351
vignesh <vigneshwar.sigamani@gmail.com>
2023-09-28 11:51:11 +0530

    questions added

---
+++ Array_Problems_Pattern/bestTimeToBuyStock.js
@@ -0,0 +1,13 @@
+// [7 (0th day), 1 (1th day), 5 (2th day) , 3 (3th day), 6 (4th day), 4 (4th day)]
+/**
+ * MAX ELEMENT = Dont buy at all
+ * ==== Options 
+ * => Selling directly if buying on the first day : 1 -> 5 (4), 1 -> 3 (2), 1 -> 6 (5), 1 -> 4 (3)
+ * 5 -> 6 (1)
+ * 3 -> 6 (3), 3 -> 4 (1)
+ * 6 -> no profit
+ * 4 -> no profit 
+ * 
+ * II
+ * => Multiple transactions starting at 1st day : 1 -> 5 (4), 3 -> 6(3) = total profit = 7
+ */
--- Array_Problems_Pattern/incrementNumberRepresentedAsArray.js
+++ Array_Problems_Pattern/incrementNumberRepresentedAsArray.js
@@ -1,4 +1,47 @@
-let m = 10;
-let n = 20;
-let count = m > n ? m : n;
-console.log(count);
+/**
+ * In a number the power of the 10 keeps on increasing when we move from rigth to left
+ 
+------ > 478 = 4 * 10 ^ 2 + 7 * 10 ^ 1 + 8 * 10 ^ 0
+1. When we are adding the numbers and the number becomes greater or equal to 10 , We will take carry as 1 and we will just write the 
+digit as the sum 
+
+ */
+
+// carry -> 1 because we need to add 1 to the number
+
+// ---------> Solution one , Returning Number 
+// let arr = [1,2,3]
+// let mulitple = 1, ans = 0 , carry = 1;
+
+// for(let i = arr.length ; i >= 0; i--){
+//     let digit = arr[i] + carry;
+//     carry = digit >= 10 ? 1 : 0;
+//     digit = digit % 10;
+//     ans = ans + digit * mulitple;
+//     mulitple = mulitple * 10;
+// }
+
+// if(carry == 1){
+//     ans = ans + mulitple;
+// }
+
+
+// ========= Number to String ============= 
+let number = 10;
+number.toString().split('')
+
+// ========== INPUT NUMBER CAN BE LARGE , THEREFORE WE NEED TO HANDLE THAT => the answer can go around 10 ^ 25 , Therefore we need to deal with the 
+//array itself 
+let arr = [1,2,3]
+let carry = 1;
+
+for(let i = arr.length - 1 ; i >= 0; i--){
+    let digit = arr[i] + carry;
+    carry = digit >= 10 ? 1 : 0;
+    digit = digit % 10;
+    arr[i] = digit;
+}
+
+if(carry == 1){
+    arr.unshift(1); // adds the element to the front of the array
+}
